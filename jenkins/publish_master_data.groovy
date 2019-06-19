ef52ed3f664e2db688eac1fa044ea79f448fbdc1
wangwei <lndl.wangwei@gmail.com>
2019-06-19 15:58:20 +0800

    jenkins test

--- jenkins/publish_master_data.groovy
+++ jenkins/publish_master_data.groovy
@@ -1,8 +1,8 @@
 pipeline {
 
-    environment {
-        scriptPath = "${WORKSPACE}/scripts/publish_master_data"
-    }
+//    environment {
+//        scriptPath = "${WORKSPACE}/scripts/publish_master_data"
+//    }
 
     agent none
 
@@ -10,17 +10,17 @@ pipeline {
         // 准备相关的脚本
         stage('make scripts executable') {
             agent {label '28test'}
+            environment {
+                scriptPath = "${WORKSPACE}/scripts/publish_master_data"
+            }
+
             steps {
                 sh "chmod +x ${scriptPath}/*"
             }
-        }
 
-        stage('generate diff sql') {
-            agent {label '28test'}
             steps {
                 sh "python $scriptPath/GenerateDiffSql.py"
             }
         }
-
     }
 }

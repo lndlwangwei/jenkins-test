7c92777e2058800477ad39f6f0a27d6787b2a13d
wangwei <lndl.wangwei@gmail.com>
2019-09-29 15:49:49 +0800

    build rbm

--- jenkins/rbm/scripts/unzip.sh
+++ jenkins/rbm/scripts/unzip.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 . ./rbm_pilot_profile
+echo "server_name $server_home"
+echo "app_name ${app_name}"
+
 serverDir=${server_home}/webapps/ROOT
 unzip -d $serverDir $serverDir/${app_name}.war
 if [ $? -eq 0 ]; then

333ccd2f5a99d9f76ce1c485938df04d0efce989
wangwei <lndl.wangwei@gmail.com>
2019-09-30 10:24:07 +0800

    build rbm

---
+++ jenkins/rbm-test/deployTestRbmConsoleWebsite.groovy
@@ -0,0 +1,14 @@
+def buildProjectName = 'rbm-console-build'
+def appDir = '/home/data/apps/rbm_client'
+def artifactName = 'rbm_test_client.zip'
+
+node('37test') {
+    stage('deploy') {
+        copyArtifacts(projectName: "${buildProjectName}")
+
+        sh "rm -rf ${appDir}/*"
+        sh "cp console-website/${artifactName} ${appDir}"
+        sh "unzip ${appDir}/${artifactName}"
+        sh "rm -f ${appDir}/${artifactName}"
+    }
+}

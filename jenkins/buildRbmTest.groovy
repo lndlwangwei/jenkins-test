36b04cfe37a4c68d847247662211ae437b666ddb
wangwei <lndl.wangwei@gmail.com>
2019-10-17 13:54:26 +0800

    build rbm prod

---
+++ jenkins/buildRbmTest.groovy
@@ -0,0 +1,19 @@
+node('28test') {
+
+    stage('build') {
+        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '1f9e7cc3-d484-4dea-bdd7-b1ed30e595d3', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+
+        sh 'mvn clean'
+        sh 'mvn install -Dmaven.test.skip=true -P test'
+
+        dir('console-website') {
+            sh 'npm install'
+            sh 'ng build --configuration=test; cd dist/; zip -r ../rbm_test_client.zip ./'
+        }
+
+        archiveArtifacts 'console-webapp/target/*.war'
+        archiveArtifacts 'file-handler-webapp/target/*.war'
+        archiveArtifacts 'api-webapp/target/*.war'
+        archiveArtifacts 'console-website/rbm_test_client.zip'
+    }
+}

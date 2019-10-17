9056c74985098d186cb8bc513efd106a40dfc04c
wangwei <lndl.wangwei@gmail.com>
2019-10-17 10:17:50 +0800

    build rbm prod

--- jenkins/buildRbm.groovy
+++ jenkins/buildRbm.groovy
@@ -9,14 +9,14 @@ node('28test') {
         checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '1f9e7cc3-d484-4dea-bdd7-b1ed30e595d3', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
 
         sh 'mvn clean'
-        sh 'mvn install -Dmaven.test.skip=true -P test'
-        sh 'mvn install -Dmaven.test.skip=true -P pilotrun'
+//        sh 'mvn install -Dmaven.test.skip=true -P test'
+//        sh 'mvn install -Dmaven.test.skip=true -P pilotrun'
         sh 'mvn install -Dmaven.test.skip=true -P product'
 
         dir('console-website') {
             sh 'npm install'
-            sh 'ng build --configuration=test; cd dist/; zip -r ../rbm_test_client.zip ./'
-            sh 'ng build --configuration=pilotrun; cd dist/; zip -r ../rbm_pilotrun_client.zip ./'
+//            sh 'ng build --configuration=test; cd dist/; zip -r ../rbm_test_client.zip ./'
+//            sh 'ng build --configuration=pilotrun; cd dist/; zip -r ../rbm_pilotrun_client.zip ./'
             sh 'ng build --configuration=prod; cd dist/; zip -r ../rbm_prod_client.zip ./'
         }
 

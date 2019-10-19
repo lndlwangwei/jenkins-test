5476ade1df24fd77291a7d492250a03a257be86e
wangwei <lndl.wangwei@gmail.com>
2019-10-19 12:28:12 +0800

    build rbm prod

--- jenkins/rbm-pilot/scripts/jetty.sh
+++ jenkins/rbm-pilot/scripts/jetty.sh
@@ -21,9 +21,9 @@ NAME=$(echo $(basename $0) | sed -e 's/^[SK][0-9]*//' -e 's/\.sh$//')
 
 env=$2
 if [ $env == 'console' ]; then
-    . $WORKSPACE/jenkins/rbm-pilot/scripts/rbm_pilot_console_profile
+    . $WORKSPACE/jenkins/rbm-pilot/scripts/rbm_console_profile
 elif [ $env == 'filehandler-pilot' ]; then
-    . $WORKSPACE/jenkins/rbm-pilot/scripts/rbm_pilot_file_handler_profile
+    . $WORKSPACE/jenkins/rbm-pilot/scripts/rbm_file_handler_profile
 fi
 
 

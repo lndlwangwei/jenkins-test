c461f4678d49dccfccef8341bd7925c439dccf56
wangwei <lndl.wangwei@gmail.com>
2020-01-06 15:15:56 +0800

    rbm正式环境jenkins部署配置

--- jenkins/buildRbmProd.groovy
+++ jenkins/buildRbmProd.groovy
@@ -4,12 +4,12 @@ node('28test') {
         checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '1f9e7cc3-d484-4dea-bdd7-b1ed30e595d3', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
 
         sh 'mvn clean'
-        sh 'mvn install -Dmaven.test.skip=true -P product'
+        sh 'mvn install -Dmaven.test.skip=true'
 
-        dir('console-website') {
-            sh 'npm install'
-            sh 'ng build --configuration=prod; cd dist/; zip -r ../rbm_prod_client.zip ./'
-        }
+//        dir('console-website') {
+//            sh 'npm install'
+//            sh 'ng build --configuration=prod; cd dist/; zip -r ../rbm_prod_client.zip ./'
+//        }
 
         dir('api-docs') {
             sh 'zip -r api-docs.zip .'
@@ -19,7 +19,7 @@ node('28test') {
         archiveArtifacts 'file-handler-webapp/target/*.jar'
         archiveArtifacts 'api-webapp/target/*.jar'
         archiveArtifacts 'api-docs/api-docs.zip'
-        archiveArtifacts 'console-website/rbm_prod_client.zip'
+//        archiveArtifacts 'console-website/rbm_prod_client.zip'
         archiveArtifacts 'sql/rbm_update.sql'
     }
 }

57499d6072f63453e686d12e9a3a4507b96a1d29
wangwei <lndl.wangwei@gmail.com>
2020-02-03 21:26:19 +0800

    正式环境的脚本增加eureka server和gateway

--- jenkins/buildRbmProd.groovy
+++ jenkins/buildRbmProd.groovy
@@ -1,4 +1,4 @@
-node('28test') {
+node('159test') {
 
     stage('build') {
         checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '1f9e7cc3-d484-4dea-bdd7-b1ed30e595d3', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])

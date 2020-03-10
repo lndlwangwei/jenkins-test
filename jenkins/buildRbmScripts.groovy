0ca29f0dd2ab4736c8f05dc4a69557bb9f1ba472
wangwei <lndl.wangwei@gmail.com>
2020-03-10 22:28:04 +0800

    脚本整合优化

--- jenkins/buildRbmScripts.groovy
+++ jenkins/buildRbmScripts.groovy
@@ -2,18 +2,18 @@ echo "env props: "
 echo "${env.WORKSPACE}, ${env.BUILD_ID}, ${env.BUILD_DISPLAY_NAME}"
 echo "${currentBuild.projectName}"
 
+def deploy = load("test1.groovy")
 
-
-node('159test') {
-    def params = [a: "wangwei", b: "15"]
-    def deploy = load("test1.groovy")
-//    deploy.execute(params)
-
-    stage('prepare scripts') {
-
-
-        git 'https://github.com/lndlwangwei/jenkins-test.git'
-
-        archiveArtifacts 'jenkins/**/*'
-    }
-}
+//node('159test') {
+//    def params = [a: "wangwei", b: "15"]
+//
+////    deploy.execute(params)
+//
+//    stage('prepare scripts') {
+//
+//
+//        git 'https://github.com/lndlwangwei/jenkins-test.git'
+//
+//        archiveArtifacts 'jenkins/**/*'
+//    }
+//}

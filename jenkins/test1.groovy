84a5832a769093b46ead3ff568f6c677e551256f
wangwei <lndl.wangwei@gmail.com>
2020-03-10 23:51:40 +0800

    脚本整合优化

--- jenkins/test1.groovy
+++ jenkins/test1.groovy
@@ -1,5 +1,13 @@
 def execute() {
     println "hello"
+
+    stage('prepare scripts') {
+        println 'prepare scripts'
+
+        git 'https://github.com/lndlwangwei/jenkins-test.git'
+
+        archiveArtifacts 'jenkins/**/*'
+    }
 }
 
 return this;

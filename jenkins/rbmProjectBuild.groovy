9ca5a4e958ae1f07dce3b8bcd3a39027f7398048
wangwei <lndl.wangwei@gmail.com>
2020-03-12 15:38:41 +0800

    jenkins测试

--- jenkins/rbmProjectBuild.groovy
+++ jenkins/rbmProjectBuild.groovy
@@ -5,8 +5,6 @@ def env = params.split(',')[1].trim()
 
 println "nodeName: ${nodeName}, env: $env"
 
-def projectName = currentBuild.projectName.split('\\(')[0]
-
 node(nodeName) {
     git 'https://github.com/lndlwangwei/jenkins-test.git'
     def build = load 'jenkins/deploy-scripts/projectBuild.groovy'

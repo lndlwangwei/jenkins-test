f5f44c545b4de27156920e1fe1f43e312e860091
wangwei <lndl.wangwei@gmail.com>
2020-03-12 16:02:50 +0800

    jenkins测试

--- jenkins/rbmProjectBuild.groovy
+++ jenkins/rbmProjectBuild.groovy
@@ -7,8 +7,8 @@ println "nodeName: ${nodeName}, env: $rbmEnv"
 
 node(nodeName) {
     git 'https://github.com/lndlwangwei/jenkins-test.git'
-    def build = load 'jenkins/deploy-scripts/projectBuild.groovy'
-
-    def envProps = [env: rbmEnv]
-    build.build(envProps)
+//    def build = load 'jenkins/deploy-scripts/projectBuild.groovy'
+//
+//    def envProps = [env: rbmEnv]
+//    build.build(envProps)
 }

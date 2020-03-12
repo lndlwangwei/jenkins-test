e92200cc59fadac3807d5705491e743fa2f18dfc
wangwei <lndl.wangwei@gmail.com>
2020-03-12 16:06:55 +0800

    jenkins测试

--- jenkins/rbmProjectBuild.groovy
+++ jenkins/rbmProjectBuild.groovy
@@ -3,7 +3,7 @@ def nodeName = params.split(',')[0].trim()
 // env: test,pilotrun,product
 def env = params.split(',')[1].trim()
 
-println "nodeName: ${nodeName}, env: $rbmEnv"
+println "nodeName: ${nodeName}, env: $env"
 
 node(nodeName) {
     git 'https://github.com/lndlwangwei/jenkins-test.git'

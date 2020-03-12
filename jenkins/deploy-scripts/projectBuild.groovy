b84f07068d285d733218bfdfd9d470226e61d211
wangwei <lndl.wangwei@gmail.com>
2020-03-12 15:50:50 +0800

    jenkins测试

--- jenkins/deploy-scripts/projectBuild.groovy
+++ jenkins/deploy-scripts/projectBuild.groovy
@@ -1,15 +1,15 @@
-build(envVar) {
-    println "inner params: $envVar"
+build(env1Var) {
+    println "inner params: $env1Var"
 
     stage('build') {
         checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '1f9e7cc3-d484-4dea-bdd7-b1ed30e595d3', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
 
         sh 'mvn clean'
-        sh "mvn install -Dmaven.test.skip=true -P ${envVar}"
+        sh "mvn install -Dmaven.test.skip=true -P ${env1Var}"
 
         dir('console-website') {
             sh 'npm install'
-            sh "ng build --configuration=${envVar}; cd dist/; zip -r ../rbm_${envVar}_client.zip ./"
+            sh "ng build --configuration=${env1Var}; cd dist/; zip -r ../rbm_${env1Var}_client.zip ./"
         }
 
         dir('api-docs') {
@@ -20,7 +20,7 @@ build(envVar) {
         archiveArtifacts 'file-handler-webapp/target/*.war'
         archiveArtifacts 'api-webapp/target/*.war'
         archiveArtifacts 'api-docs/api-docs.zip'
-        archiveArtifacts "console-website/rbm_${envVar}_client.zip"
+        archiveArtifacts "console-website/rbm_${env1Var}_client.zip"
     }
 }
 

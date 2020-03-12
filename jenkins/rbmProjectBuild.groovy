56aa68aa98d62fcd85dfe5276fa8a5b9bd7bec69
wangwei <lndl.wangwei@gmail.com>
2020-03-13 00:41:23 +0800

    jenkins测试

--- jenkins/rbmProjectBuild.groovy
+++ jenkins/rbmProjectBuild.groovy
@@ -3,16 +3,18 @@
 // env: test,pilotrun,product
 //def env = params.split('-')[1].trim()
 def env = 'test'
-checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_credential', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
-git 'https://github.com/lndlwangwei/jenkins-test.git'
+def build;
+
+
 
 //println "nodeName: ${nodeName}, env: $env"
 
 node('159test') {
+    checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_credential', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+    git 'https://github.com/lndlwangwei/jenkins-test.git'
 
 
-
-    def build = load 'jenkins/deploy-scripts/projectBuild.groovy'
+    build = load 'jenkins/deploy-scripts/projectBuild.groovy'
 
 //    def envProps = [env: rbmEnv]
     build.build(env)

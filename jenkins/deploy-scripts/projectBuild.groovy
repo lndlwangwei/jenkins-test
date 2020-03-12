b6220a34e8efa4850f26e610739fcd896a7251c8
wangwei <lndl.wangwei@gmail.com>
2020-03-12 15:39:10 +0800

    jenkins测试

--- jenkins/deploy-scripts/projectBuild.groovy
+++ jenkins/deploy-scripts/projectBuild.groovy
@@ -5,11 +5,11 @@ build(env) {
         checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '1f9e7cc3-d484-4dea-bdd7-b1ed30e595d3', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
 
         sh 'mvn clean'
-        sh "mvn install -Dmaven.test.skip=true -P ${envProps.env}"
+        sh "mvn install -Dmaven.test.skip=true -P ${env}"
 
         dir('console-website') {
             sh 'npm install'
-            sh "ng build --configuration=${envProps}; cd dist/; zip -r ../rbm_${env}_client.zip ./"
+            sh "ng build --configuration=${env}; cd dist/; zip -r ../rbm_${env}_client.zip ./"
         }
 
         dir('api-docs') {

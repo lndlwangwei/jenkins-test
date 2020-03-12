5f7c5f274dbe00f77ffe871ee2e88527664fce21
wangwei <lndl.wangwei@gmail.com>
2020-03-12 16:05:58 +0800

    jenkins测试

--- jenkins/deploy-scripts/projectBuild.groovy
+++ jenkins/deploy-scripts/projectBuild.groovy
@@ -1,15 +1,15 @@
-def build(envProps) {
-    println "inner params: ${envProps}"
+def build(env) {
+    println "inner params: ${env}"
 
     stage('build') {
         checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '1f9e7cc3-d484-4dea-bdd7-b1ed30e595d3', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
 
         sh 'mvn clean'
-        sh "mvn install -Dmaven.test.skip=true -P ${envProps.env}"
+        sh "mvn install -Dmaven.test.skip=true -P ${env}"
 
         dir('console-website') {
             sh 'npm install'
-            sh "ng build --configuration=${envProps.env}; cd dist/; zip -r ../rbm_${envProps.env}_client.zip ./"
+            sh "ng build --configuration=${env}; cd dist/; zip -r ../rbm_${env}_client.zip ./"
         }
 
         dir('api-docs') {
@@ -20,7 +20,7 @@ def build(envProps) {
         archiveArtifacts 'file-handler-webapp/target/*.war'
         archiveArtifacts 'api-webapp/target/*.war'
         archiveArtifacts 'api-docs/api-docs.zip'
-        archiveArtifacts "console-website/rbm_${envProps.envF}_client.zip"
+        archiveArtifacts "console-website/rbm_${env}_client.zip"
     }
 }
 
--- jenkins/rbmProjectBuild.groovy
+++ jenkins/rbmProjectBuild.groovy
@@ -1,14 +1,14 @@
 def params = currentBuild.projectName.find(/\(([^\)]+)\)/, {group -> group[1]})
 def nodeName = params.split(',')[0].trim()
 // env: test,pilotrun,product
-def rbmEnv = params.split(',')[1].trim()
+def env = params.split(',')[1].trim()
 
 println "nodeName: ${nodeName}, env: $rbmEnv"
 
 node(nodeName) {
     git 'https://github.com/lndlwangwei/jenkins-test.git'
     def build = load 'jenkins/deploy-scripts/projectBuild.groovy'
-//
+
 //    def envProps = [env: rbmEnv]
-//    build.build(envProps)
+    build.build(env)
 }

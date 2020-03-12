36a40fd821d445ee8d92345aa69328c93fba70c5
wangwei <lndl.wangwei@gmail.com>
2020-03-12 23:50:59 +0800

    jenkins测试

--- jenkins/deploy-scripts/projectBuild.groovy
+++ jenkins/deploy-scripts/projectBuild.groovy
@@ -2,7 +2,7 @@ def build(env) {
     println "inner params: ${env}"
 
     stage('build') {
-        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_credential', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+        // checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_credential', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
 
         sh 'mvn clean'
         sh "mvn install -Dmaven.test.skip=true -P ${env}"

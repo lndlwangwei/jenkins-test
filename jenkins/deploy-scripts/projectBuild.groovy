6b486cb8b6266cdafda24cfaa40bcae999deb9d9
wangwei <lndl.wangwei@gmail.com>
2020-03-12 15:51:55 +0800

    jenkins测试

--- jenkins/deploy-scripts/projectBuild.groovy
+++ jenkins/deploy-scripts/projectBuild.groovy
@@ -1,5 +1,5 @@
 build(env1Var) {
-    println "inner params: $env1Var"
+    println "inner params: ${env1Var}"
 
     stage('build') {
         checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '1f9e7cc3-d484-4dea-bdd7-b1ed30e595d3', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://114.55.64.147/svn/rbm/trunk']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])

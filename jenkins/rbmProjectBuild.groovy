7cb271a4382a11aff1b3a4b29ccfff4f80ab8929
wangwei <lndl.wangwei@gmail.com>
2020-03-12 15:52:57 +0800

    jenkins测试

--- jenkins/rbmProjectBuild.groovy
+++ jenkins/rbmProjectBuild.groovy
@@ -9,5 +9,5 @@ node(nodeName) {
     git 'https://github.com/lndlwangwei/jenkins-test.git'
     def build = load 'jenkins/deploy-scripts/projectBuild.groovy'
 
-    build.build(rbmEnv)
+//    build.build(rbmEnv)
 }

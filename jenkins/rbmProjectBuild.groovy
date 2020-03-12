2a522fa0a817343b1479bf79e4255d9136967140
wangwei <lndl.wangwei@gmail.com>
2020-03-12 15:44:49 +0800

    jenkins测试

--- jenkins/rbmProjectBuild.groovy
+++ jenkins/rbmProjectBuild.groovy
@@ -1,13 +1,13 @@
 def params = currentBuild.projectName.find(/\(([^\)]+)\)/, {group -> group[1]})
 def nodeName = params.split(',')[0].trim()
 // env: test,pilotrun,product
-def env = params.split(',')[1].trim()
+def rbmEnv = params.split(',')[1].trim()
 
-println "nodeName: ${nodeName}, env: $env"
+println "nodeName: ${nodeName}, env: $rbmEnv"
 
 node(nodeName) {
     git 'https://github.com/lndlwangwei/jenkins-test.git'
     def build = load 'jenkins/deploy-scripts/projectBuild.groovy'
 
-    build.build(env)
+    build.build(rbmEnv)
 }

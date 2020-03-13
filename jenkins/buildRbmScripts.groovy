1cd3f59504944f68d37bdbc1f8b17ecd8fba3edc
wangwei <lndl.wangwei@gmail.com>
2020-03-13 14:18:21 +0800

    jenkins测试

--- jenkins/buildRbmScripts.groovy
+++ jenkins/buildRbmScripts.groovy
@@ -1,26 +1,8 @@
-//def nodeName = currentBuild.projectName.find(/\(([^\)]+)\)/, {group -> group[1]})
+def nodeName = currentBuild.projectName.find(/\(([^\)]+)\)/, {group -> group[1]})
 
-node('159test') {
-//    git 'https://github.com/lndlwangwei/jenkins-test.git'
-//    deploy = load 'jenkins/test1.groovy'
-
+node(nodeName) {
     stage('prepare scripts') {
         git 'https://github.com/lndlwangwei/jenkins-test.git'
         archiveArtifacts 'jenkins/**/*'
     }
 }
-
-
-//node('159test') {
-//    def params = [a: "wangwei", b: "15"]
-//
-////    deploy.execute(params)
-//
-//    stage('prepare scripts') {
-//
-//
-//        git 'https://github.com/lndlwangwei/jenkins-test.git'
-//
-//        archiveArtifacts 'jenkins/**/*'
-//    }
-//}

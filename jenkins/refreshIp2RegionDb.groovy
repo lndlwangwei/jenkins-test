d5229533c53b4e99274b3b307a7a7aa98c48068e
wangwei <lndl.wangwei@gmail.com>
2020-08-25 13:41:24 +0800

    refresh ip2region jenkins test

--- jenkins/refreshIp2RegionDb.groovy
+++ jenkins/refreshIp2RegionDb.groovy
@@ -1,5 +1,7 @@
 node(params.NODE_NAME) {
     stage('download ip2region db') {
-        sh 'wget -O /data/wangwei/ip2region.db https://gitee.com/lionsoul/ip2region/raw/master/data/ip2region.db'
+//        sh 'wget -O /data/wangwei/ip2region.db https://gitee.com/lionsoul/ip2region/raw/master/data/ip2region.db'
+
+        git 'https://gitee.com/lionsoul/ip2region.git'
     }
 }
